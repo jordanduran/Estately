b881dc16c4f1e4773a65038bfaa2cad02ccd026d
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2021-12-09 12:05:27 -0500

    Contact landlord page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
@@ -32,6 +33,7 @@ const App = () => {
             path='/category/:categoryName/:listingId'
             element={<Listing />}
           />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
